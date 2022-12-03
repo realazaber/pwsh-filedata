d89f88a3f05dc30f755685a7ecbcc297f788d741
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2022-12-03 05:15:58 +0100

    Added file path parameter

--- main.ps1
+++ main.ps1
@@ -1,15 +1,11 @@
 $filePath = $args[0]
 
-
 if ( $filePath -ne $null)
 {
-    Get-ChildItem * -File | select Name, CreationTime, LastAccessTime, LastWriteTime, @{Label='HashValue';Expression={(Get-FileHash $_).Hash}}| Export-Csv -Path "main.csv"
+    Get-ChildItem $filePath * -File | select Name, CreationTime, LastAccessTime, LastWriteTime, @{Label='HashValue'; Expression={(Get-FileHash $_).Hash}}| Export-Csv -Path "main.csv";
     Write-Output "Data saved!";
 }
 else 
 {
     Write-Output "Please add a file path";
 }
-
-
-
