7e57c334c619a6fcd6c3da561030d9c0b310b320
swagoverlord <azcooldude8@gmail.com>
2022-12-03 03:13:24 +0100

    Show all data in current directory

--- main.ps1
+++ main.ps1
@@ -2,4 +2,4 @@ $filePath = $args[0]
 
 Get-FileHash $filePath *
 
-Get-ChildItem * | select Name, CreationTime, LastAccessTime, LastWriteTime
+Get-FileHash *; Get-ChildItem * | select Name, CreationTime, LastAccessTime, LastWriteTime
