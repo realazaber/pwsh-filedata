a13ac6ff6a6a6523b7b399f306333790e49a3265
swagoverlord <azcooldude8@gmail.com>
2022-12-03 05:12:36 +0100

    Require a file path

--- main.ps1
+++ main.ps1
@@ -1,5 +1,15 @@
-# $filePath = $args[0]
+$filePath = $args[0]
 
-# Get-FileHash $filePath *
 
-Get-FileHash * | Export-Csv -Path "hashs.csv"; Get-ChildItem * | select Name, CreationTime, LastAccessTime, LastWriteTime | Export-Csv -Path "main.csv"
+if ( $filePath -ne $null)
+{
+    Get-FileHash * | Export-Csv -Path "main.csv"; 
+    Get-ChildItem * | select Name, CreationTime, LastAccessTime, LastWriteTime | Export-Csv -Append -Path "main.csv" -Force;
+    Write-Output "Data saved!";
+}
+else 
+{
+    Write-Output "Please add a file path";
+}
+
+
